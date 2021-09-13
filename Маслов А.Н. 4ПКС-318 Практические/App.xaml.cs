dc725be9f0c0f8b09ce19e20dcbd83711393b23f
Aknas Macefg <alex.maslov14@gmail.com>
2021-09-13 12:41:48 +0300

    Добавьте файлы проекта.

---
+++ Маслов А.Н. 4ПКС-318 Практические/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Маслов_А.Н._4ПКС_318_Практические
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
