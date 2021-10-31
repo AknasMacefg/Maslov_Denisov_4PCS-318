f92725195585ab4540b04da642110b2b1c0befba
Aknas Macefg <alex.maslov14@gmail.com>
2021-10-31 21:36:25 +0300

    Update to PR#8

--- Маслов А.Н. 4ПКС-318 Практические/Pages/ProductsForCustomerPage.xaml.cs
+++ Маслов А.Н. 4ПКС-318 Практические/Pages/ProductsForCustomerPage.xaml.cs
@@ -23,8 +23,95 @@ namespace Маслов_А.Н._4ПКС_318_Практические.Pages
         public ProductsForCustomerPage()
         {
             InitializeComponent();
-            var currentProducts = Entities.GetContext().Products.ToList();
-            ListUser.ItemsSource = currentProducts;
+            SortPrice.SelectedIndex = 0;
+            OnlyGPU.IsChecked = false;
+            OnlyRAM.IsChecked = false;
+            OnlyProcessors.IsChecked = false;
+
+            var currentUsers = Entities.GetContext().Products.ToList();
+            ListUser.ItemsSource = currentUsers;
+            UpdateUsers();
+        }
+
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+
+  
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void UpdateUsers()
+        {
+
+            var currentUsers = Entities.GetContext().Products.ToList();
+
+
+            currentUsers = currentUsers.Where(x => x.Name.ToLower().Contains(SearchText.Text.ToLower())).ToList();
+
+            if (OnlyRAM.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Name.Contains("ОЗУ")).ToList();
+            if (OnlyGPU.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Name.Contains("Видеокарта")).ToList();
+            if (OnlyProcessors.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Name.Contains("Процессор")).ToList();
+
+            if (SortPrice.SelectedIndex == 1)
+                ListUser.ItemsSource = currentUsers.OrderBy(x => x.Price).ToList();
+            else ListUser.ItemsSource = currentUsers.OrderByDescending(x => x.Price).ToList();
+        }
+
+        private void ClearAll()
+        {
+            SearchText.Text = "";
+            SortPrice.SelectedIndex = 0;
+            OnlyGPU.IsChecked = false;
+            OnlyRAM.IsChecked = false;
+            OnlyProcessors.IsChecked = false;
+            UpdateUsers();
+        }
+
+
+    
+
+        private void OnlyRAM_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void OnlyRAM_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void OnlyGPU_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void OnlyGPU_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void OnlyProcessors_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void OnlyProcessors_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void SortPrice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUsers();
         }
     }
        
--- Маслов А.Н. 4ПКС-318 Практические/Pages/UserPage.xaml.cs
+++ Маслов А.Н. 4ПКС-318 Практические/Pages/UserPage.xaml.cs
@@ -23,8 +23,94 @@ namespace Маслов_А.Н._4ПКС_318_Практические.Pages
         public UserPage()
         {
             InitializeComponent();
+            SortType.SelectedIndex = 0;
+            Customers.IsChecked = false;
+            Sellers.IsChecked = false;
+            Admins.IsChecked = false;
             var currentUsers = Entities.GetContext().User.ToList();
             ListUser.ItemsSource = currentUsers;
+            UpdateUsers();
+        }
+
+        private void SearchText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void SortType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        
+
+        private void Customers_Checked(object sender, RoutedEventArgs e)
+        {
+          
+            UpdateUsers();
+        }
+
+        private void Sellers_Checked(object sender, RoutedEventArgs e)
+        {
+           
+            UpdateUsers();
+        }
+
+        private void Admins_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            UpdateUsers();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void UpdateUsers()
+        {
+
+            var currentUsers = Entities.GetContext().User.ToList();
+
+
+            currentUsers = currentUsers.Where(x => x.FIO.ToLower().Contains(SearchText.Text.ToLower())).ToList();
+
+            if (Customers.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Role.Contains("Customer")).ToList();
+            if (Sellers.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Role.Contains("Seller")).ToList();
+            if (Admins.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Role.Contains("Director")).ToList();
+
+
+            if (SortType.SelectedIndex == 0)
+                ListUser.ItemsSource = currentUsers.OrderBy(x => x.FIO).ToList();
+            else ListUser.ItemsSource = currentUsers.OrderByDescending(x => x.FIO).ToList();
+        }
+
+        private void ClearAll()
+        {
+            SearchText.Text = "";
+            SortType.SelectedIndex = 0;
+            Customers.IsChecked = false;
+            Sellers.IsChecked = false;
+            Admins.IsChecked = false;
+            UpdateUsers();
+        }
+
+        private void Admins_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void Sellers_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void Customers_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();
         }
     }
-}
+    }
